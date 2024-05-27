9594ff64088da536a13a082fce792247ef9cd281
magdalenamakarewicz <makarewicz.magdalena@outlook.com>
2024-05-27 13:58:35 +0200

    Clean up unused variables and format code

--- src/main/java/com/enjoythecode/childrensmokinganalysis/app/Main.java
+++ src/main/java/com/enjoythecode/childrensmokinganalysis/app/Main.java
@@ -6,7 +6,6 @@ import com.enjoythecode.childrensmokinganalysis.model.Smoke;
 import com.enjoythecode.childrensmokinganalysis.service.ChildService;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,7 +15,6 @@ public class Main {
     public static void main(String[] args) {
 
         String filePath = "children_smoking.txt";
-        File file = new File(filePath);
 
         List<Child> children = readChildrenFromFile(filePath);
 
@@ -59,7 +57,7 @@ public class Main {
 
     private static Child getChild(String nextLine) {
         String[] values = nextLine.split(",");
-        Child child = new Child(
+        return new Child(
                 Integer.parseInt(values[0]),
                 Integer.parseInt(values[1]),
                 Integer.parseInt(values[2]),
@@ -68,7 +66,6 @@ public class Main {
                 values[5].equals("1") ? Sex.MALE : Sex.FEMALE,
                 values[6].equals("1") ? Smoke.YES : Smoke.NO
         );
-        return child;
     }
 
 }
